bc2068137eb7863a83d64a211719557b1ad27013
Shahzad Qureshi <30411645+ShahzadQ@users.noreply.github.com>
2024-07-23 19:41:13 +0100

    feat: change bundler and some code cleanup

--- src/index.ts
+++ src/index.ts
@@ -3,31 +3,25 @@ import type { CookieAttributes } from "js-cookie";
 
 import Cookies from "js-cookie";
 
-const CookieStorage: (options?: CookieAttributes) => StateStorage = (
-  options = {
+// https://docs.pmnd.rs/zustand/integrations/persisting-store-data#how-can-i-use-a-custom-storage-engine
+
+const CookieStorage= (
+  options: CookieAttributes = {
     expires: new Date(new Date().setFullYear(9999)),
   }
 ) => {
-  // return a StateStorage object with the required functions
   return {
     getItem: async (name: string): Promise<string | null> => {
-      // attempt to get the cookie
       const cookie = Cookies.get(name);
-
-      // if the cookie exists return it
-      //    otherise return null
       return typeof cookie !== "undefined" ? cookie : null;
     },
     setItem: async (name: string, value: string): Promise<void> => {
-      // set a cookie with options
       await Cookies.set(name, value, options);
     },
     removeItem: async (name: string): Promise<void> => {
-      // delete a cookie with a matching name and
-      //    matching options
       await Cookies.remove(name, options);
     },
-  };
+  } as StateStorage;
 };
 
 export default CookieStorage;
